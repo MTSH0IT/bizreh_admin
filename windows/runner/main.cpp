9e6cc7ca2fde2b20761442a3bb3c47ca1afa84ec
MTSH0IT <malshurbaji.11@gmail.com>
2026-05-30 15:33:10 +0300

    feat: configure project metadata and add cross-platform application icons

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -27,7 +27,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
   Win32Window::Size size(1280, 720);
-  if (!window.Create(L"bizreh_admin", origin, size)) {
+  if (!window.Create(L"Admin BIzreh Paints", origin, size)) {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
